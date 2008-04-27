384da27a2dbdca29b4b9474a27a3b5bff8e3c3cb
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-04-26 19:48:33 -0600

    Make tooltip writer handle non-string titles  * This will allow for multiple no-title sources

--- Engravings.lua
+++ Engravings.lua
@@ -27,7 +27,7 @@ local R, G, B = 1, 136/255, 0
 local function OnTooltipSetItem(frame, ...)
 	local name, link = frame:GetItem()
 	local id = tonumber(link:match("item:(%d+):"))
-	for title,data in pairs(sources) do if data[id] then frame:AddDoubleLine(title, data[id], R, G, B, R, G, B) end end
+	for title,data in pairs(sources) do if data[id] then frame:AddDoubleLine(type(title) == "string" and title or "", data[id], R, G, B, R, G, B) end end
 	if origs[frame] then return origs[frame](frame, ...) end
 end
 for _,frame in pairs{GameTooltip, ItemRefTooltip} do
