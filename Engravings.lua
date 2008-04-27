3f6bc1505186fc4cc53ea5985fec6bca1257453e
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-04-26 20:12:34 -0600

    Make presets use new multiple source system

--- Engravings.lua
+++ Engravings.lua
@@ -1,35 +1,18 @@
 
-local presets = Engravings_presets
-Engravings_presets = nil
-
-
 Engravings = {}
 local sources = Engravings
-
-local p = presets
-presets = {}
-
-local f = function(v, ...)
-	for i=1,select("#", ...) do
-		local id = select(i, ...)
-		presets[tonumber(id)] = v
-	end
-end
-
-for i,v in pairs(p) do
-	if type(i) == "string" then f(v, string.split(" ", i))
-	else presets[i] = v end
-end
-
-
 local origs = {}
 local R, G, B = 1, 136/255, 0
+
+
 local function OnTooltipSetItem(frame, ...)
 	local name, link = frame:GetItem()
 	local id = tonumber(link:match("item:(%d+):"))
 	for title,data in pairs(sources) do if data[id] then frame:AddDoubleLine(type(title) == "string" and title or "", data[id], R, G, B, R, G, B) end end
 	if origs[frame] then return origs[frame](frame, ...) end
 end
+
+
 for _,frame in pairs{GameTooltip, ItemRefTooltip} do
 	origs[frame] = frame:GetScript("OnTooltipSetItem")
 	frame:SetScript("OnTooltipSetItem", OnTooltipSetItem)
--- EngravingsPresets.lua
+++ EngravingsPresets.lua
@@ -1,54 +1,67 @@
 
-Engravings_presets = {
-	[10450] = "This item has no use, vendor it!",
+---------------------------------------
+--      Incorrect recipe colors      --
+---------------------------------------
 
-	---- Librams ----
-	[11732] = "+150MP Head/Leg enchant\nBurning Steppes",
-	[11733] = "+100HP Head/Leg enchant\nBurning Steppes",
-	[11734] = "+125 Armor Head/Leg enchant\nBurning Steppes",
-	[11736] = "+20 Fire Resistance Head/Leg enchant\nBurning Steppes",
-	[11737] = "+8 Any Stat Head/Leg enchant\nBurning Steppes",
-	[18332] = "+1% Haste Head/Leg enchant\nDire Maul",
-	[18333] = "+8 Healing/Damage Head/Leg enchant\nDire Maul",
-	[18334] = "+1% Dodge Head/Leg enchant\nDire Maul",
+local VENDOR_RECIPES = " 12703 23151 288291 23147 23153 6377 11223 11163 11101 6346 6342 11039 7451 7361 5789 8409 13288 13287 5788 9301 14634 23574 "
+local DROPPED_RECIPES = " 3393 3394 3931 6211 17709 "
+Engravings["Recipe source:"] = setmetatable({}, {
+	__index = function(t,i)
+		if VENDOR_RECIPES:match(" "..i.." ") then t[i] = "Vendor"; return "Vendor"
+		elseif DROPPED_RECIPES:match(" "..i.." ") then t[i] = "Mob-drop"; return "Mob-drop"
+		else t[i] = false; return end
+	end
+})
 
-	---- ZG Coin sets ----
-	["19698 19699 19700"] = "Zulian - Razzashi - Hakkari",
-	["19701 19702 19703"] = "Gurubashi - Vilebranch - Witherbark",
-	["19704 19705 19706"] = "Sandfury - Skullsplitter - Bloodscalp",
 
-	---- Darkmoon Cards ----
-	["19227 19228 19230 19231 19232 19233 19234 19235 19236"] = "Blue Dragon - 2% chance of 15sec 100% mana regen while casting",
-	["19257 19258 19259 19260 19261 19262 19263 19264 19265"] = "Heroism - Chance on deal melee damage heal player 120-180 HP",
-	["19276 19277 19278 19279 19280 19281 19282 19283 19284"] = "Twisting Nether - 10% chance to rez with 20% HP/MP",
-	["19267 19268 19269 19270 19271 19272 19273 19274 19275"] = "Maelstrom - Melee chance deal 200-300 Nature damage",
+----------------------------------
+--      Faction currencies      --
+----------------------------------
 
-	---- Currency ----
-	[24579] = "Currency - Honor Hold", -- Mark of Honor Hold
-	[24581] = "Currency - Thrallmar", -- Mark of Thrallmar
-	[24245] = "Currency - Sporeggar\nFaction Rep - Sporeggar (x10)", -- Glowcap
-	[29735] = "Currency - The Aldor", -- Holy Dust
-	[29736] = "Currency - The Scryers", -- Arcane Rune
-	[26045] = "Currency - Halaa", -- Halaa Battle Token
-	[26044] = "Currency - Halaa", -- Halaa Research Token
+local CURRENCIES = [[
+24579 Honor Hold
+24581 Thrallmar
+24245 Sporeggar
+29735 The Aldor
+29736 The Scryers
+26045 Halaa
+26044 Halaa
+]]
+Engravings["Currency:"] = setmetatable({}, {
+	__index = function(t,i)
+		local v = CURRENCIES:match("\n"..i.." ([^\n]+)\n")
+		if v then t[i] = v; return v
+		else t[i] = false; return end
+	end
+})
 
-	---- Rep Tokens ----
-	[24401] = "Faction Rep - Cenarion Expedition (x10)", -- Unidentified Plant Parts
-	[24449] = "Faction Rep - Sporeggar (x10)", -- Fertile Spores
-	[24291] = "Faction Rep - Sporeggar (x6)",  -- Bog Lord Tendril
-	[29425] = "Faction Rep - The Aldor (x10)", -- Mark of Kil'jaeden
-	[30809] = "Faction Rep - The Aldor (x10)", -- Mark of Sargeras
-	[29740] = "Faction Rep - The Aldor\nReward - Holy Dust",     -- Fel Armament
-	[29739] = "Faction Rep - The Scryers\nReward - Arcane Rune", -- Arcane Tome
-	[29426] = "Faction Rep - The Scryers (x10)", -- Firewing Signet
-	[30810] = "Faction Rep - The Scryers (x10)", -- Sunfury Signet
-	[25463] = "Faction Rep - Consortium (x3)\nUntil friendly", -- Pair of Ivory Tusks
-	[25433] = "Faction Rep - Consortium (x10)\nFaction Rep - " .. (UnitFactionGroup("player") == "Alliance" and "Kurenai" or "Mag'har").. " (x10)", -- Obsidian Warbeads
-	[25719] = "Faction Rep - Lower City (x30)\nUntil honored", -- Arakkoa Feather
 
-	---- Incorrect recipe colors ----
-	["12703 23151 288291 23147 23153 6377 11223 11163 11101 6346 6342 11039 7451 7361 5789 8409 13288 13287 5788 9301 14634 23574"] = "Vendor recipe",
-	["3393 3394 3931 6211 17709"] = "Mob-dropped recipe",
-}
+---------------------------------
+--      Reputation tokens      --
+---------------------------------
+
+local TOKENS = [[
+24401 Cenarion Expedition (250 per 10)
+24449 Sporeggar (750 per 10)
+24291 Sporeggar (750 per 6)
+29425 The Aldor (25 each)
+30809 The Aldor (25 each)
+29740 The Aldor (350 each)
+29739 The Scryers (350 each)
+29426 The Scryers (25 each)
+30810 The Scryers (25 each)
+25463 Consortium (250 per 3)
+29209 Consortium (250 per 10)
+25433 Consortium, Mag'har, Kurenai (250/500/500 per 10)
+25719 Lower City (250 per 30)
+24245 Sporeggar (75 per 10)
+]]
+Engravings["Faction Rep:"] = setmetatable({}, {
+	__index = function(t,i)
+		local v = TOKENS:match("\n"..i.." ([^\n]+)\n")
+		if v then t[i] = v; return v
+		else t[i] = false; return end
+	end
+})
 
 
