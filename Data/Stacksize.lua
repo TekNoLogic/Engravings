97801dfba41d668125d23b158e75b9a2f840abc7
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-04-28 12:39:01 -0600

    Fix nil error with stack size module

--- Data/Stacksize.lua
+++ Data/Stacksize.lua
@@ -2,7 +2,7 @@
 Engravings["Stack size:"] = setmetatable({}, {
 	__index = function(t,i)
 		local _, _, _, _, _, _, _, stack = GetItemInfo(i)
-		stack = stack > 1 and stack or false
+		stack = (stack or 0) > 1 and stack or false
 		t[i] = stack
 		return stack
 	end
