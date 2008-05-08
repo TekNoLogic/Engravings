fa21eae959669174368e34770aa8f11fe6cbfb72
Tekkub Stoutwrithe <tekkub@gmail.com>
2008-05-08 15:39:56 -0600

    GetItem can return nil, joy...

--- Engravings.lua
+++ Engravings.lua
@@ -7,8 +7,10 @@ local R, G, B = 1, 136/255, 0
 
 local function OnTooltipSetItem(frame, ...)
 	local name, link = frame:GetItem()
-	local id = tonumber(link:match("item:(%d+):"))
-	for title,data in pairs(sources) do if data[id] then frame:AddDoubleLine(type(title) == "string" and title or "", data[id], R, G, B, R, G, B) end end
+	if link then
+		local id = tonumber(link:match("item:(%d+):"))
+		for title,data in pairs(sources) do if data[id] then frame:AddDoubleLine(type(title) == "string" and title or "", data[id], R, G, B, R, G, B) end end
+	end
 	if origs[frame] then return origs[frame](frame, ...) end
 end
 
