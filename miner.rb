58efa88b9dd2eb03e1b3e504c3447a89928390ef
Tekkub <tekkub@gmail.com>
2008-06-14 13:48:25 -0600

    Add price parser, PvP Season prices

--- miner.rb
+++ miner.rb
@@ -112,6 +112,21 @@ def parse_npc(http, npc_id, zone_drops)
 end
 
 
+def parse_prices(body)
+	return [] unless body =~ /id: 'sells'(.*)/
+	items = $&.gsub(/sourcemore:\[[^\]]+\]/, "").scan(/id:\d+,[^\}]+\}/).map {|str| [$1, $2.gsub(/[\[\]]/, "").split(",")] if str =~ /id:(\d+),.+cost:\[(.*)\]\}/}
+	icons = items.map {|id,cost| cost[3]}.uniq.map {|id| [id, "Interface\\Icons\\" + $1] if body =~ /\[#{id}\]=\{icon:'([^;]+)'\};/}
+	items.map do |id,cost|
+		str = id
+		str += " #{cost[0]} gold" if cost[0].to_i > 0
+		str += " #{cost[1].to_i.abs} |TInterface\\TargetingFrame\\UI-PVP-FFA:40:40:0:-8|t" if cost[1].to_i.abs > 0
+		str += " #{cost[2]} |TInterface\\PVPFrame\\PVP-ArenaPoints-Icon:14|t" if cost[2].to_i > 0
+		str += " #{cost[4]} |T#{icons.assoc(cost[3])[1]}:18|t" if cost[4]
+		str
+	end
+end
+
+
 def mine_drops(http)
 	zone_ids = []
 
@@ -192,10 +207,26 @@ def mine_raid_tokens(http)
 end
 
 
+def mine_pvp_prices(http)
+	puts "\nQuerying PvP rewards"
+
+	pvp_rewards = []
+	vendors = [
+		"/?npc=24392", # Season 3
+		"/?npc=23396", # Season 2
+		"/?npc=20278", # Season 1
+	].each {|vendor| pvp_rewards += parse_prices http.get(vendor).body}
+
+	export(File.join("Data", "PvPCosts.lua"), "PVP_COSTS", "PvP:", pvp_rewards.sort.join("\n"))
+	puts "PvP rewards added, #{pvp_rewards.size} items imported."
+end
+
+
 Net::HTTP.start("www.wowhead.com") do |http|
 	mine_drops http
 	mine_boj http
 	mine_raid_tokens http
+	mine_pvp_prices http
 end
 
 
