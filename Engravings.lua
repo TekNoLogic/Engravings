79bf4cf89334214c65fb1d49ff4cf98fd1193507
Tekkub <tekkub@gmail.com>
2009-03-18 02:31:14 -0600

    Add config panel

--- Engravings.lua
+++ Engravings.lua
@@ -3,13 +3,15 @@ Engravings = {}
 local sources = Engravings
 local origs = {}
 local R, G, B = 1, 136/255, 0
+local db
 
 
 local function OnTooltipSetItem(frame, ...)
+	if not db then EngravingsDB2 = EngravingsDB2 or {}; db = EngravingsDB2 end
 	local name, link = frame:GetItem()
 	if link then
 		local id = tonumber(link:match("item:(%d+):"))
-		for title,data in pairs(sources) do if data[id] then frame:AddDoubleLine(type(title) == "string" and title or "", data[id], R, G, B, R, G, B) end end
+		for title,data in pairs(sources) do if not db[title] and data[id] then frame:AddDoubleLine(type(title) == "string" and title or "", data[id], R, G, B, R, G, B) end end
 	end
 	if origs[frame] then return origs[frame](frame, ...) end
 end
@@ -19,5 +21,3 @@ for _,frame in pairs{GameTooltip, ItemRefTooltip, ShoppingTooltip1, ShoppingTool
 	origs[frame] = frame:GetScript("OnTooltipSetItem")
 	frame:SetScript("OnTooltipSetItem", OnTooltipSetItem)
 end
-
-
