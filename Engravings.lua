7a5dcbee96b219faee6021cb99527675ea831598
Tekkub <tekkub@gmail.com>
2009-03-22 01:47:38 -0600

    Sort the tooltip displays

--- Engravings.lua
+++ Engravings.lua
@@ -3,15 +3,23 @@ Engravings = {}
 local sources = Engravings
 local origs = {}
 local R, G, B = 1, 136/255, 0
-local db
-
+local db, sortedtitles
 
 local function OnTooltipSetItem(frame, ...)
 	if not db then EngravingsDB2 = EngravingsDB2 or {}; db = EngravingsDB2 end
+	if not sortedtitles then
+		sortedtitles = {}
+		for title,data in pairs(sources) do table.insert(sortedtitles, title) end
+		table.sort(sortedtitles, function(a,b) return string.lower(a) < string.lower(b) end)
+	end
+
 	local name, link = frame:GetItem()
 	if link then
 		local id = tonumber(link:match("item:(%d+):"))
-		for title,data in pairs(sources) do if not db[title] and data[id] then frame:AddDoubleLine(type(title) == "string" and title or "", data[id], R, G, B, R, G, B) end end
+		for i,title in pairs(sortedtitles) do
+			local data = sources[title]
+			if not db[title] and data[id] then frame:AddDoubleLine(type(title) == "string" and title or "", data[id], R, G, B, R, G, B) end
+		end
 	end
 	if origs[frame] then return origs[frame](frame, ...) end
 end
