460d23c5efa465e9c95346c0b0bffe143434c2ea
Tekkub <tekkub@gmail.com>
2009-03-29 15:55:48 -0600

    Again with the underscore

--- Data/WowheadHunterBeast_mastery.lua
+++ Data/WowheadHunterBeast_mastery.lua
@@ -4771,7 +4771,7 @@ local WOWHEAD_SCORE = [[
 44935 2.069
 ]]
 
-Engravings["Wowhead score (Beast_mastery):"] = setmetatable({}, {
+Engravings["Wowhead score (Beast mastery):"] = setmetatable({}, {
 	__index = function(t,i)
 		local v = WOWHEAD_SCORE:match("\n"..i.." ([^\n]+)\n")
 		if v then t[i] = v; return v
