26e9ed3c2fe4ef2c05813661e883a4b26e9b0941
tekkub <tekkub@gmail.com>
2011-01-04 03:41:07 -0700

    idols/librams/totems/sigils are gone, is relics now

--- Miner/weight_miner.rb
+++ Miner/weight_miner.rb
@@ -11,11 +11,10 @@ armor_slots = [1, 3, 5, 6, 7, 8, 9, 10]
 items = {
   :common => ["4.-6&filter=", "4.-5&filter=", "4.-3&filter=", "4.-2&filter=", "4.-4&filter="],
   :offhand => ["4.-5&filter="], :shield => ["4.6&filter="],
-  :idol => ["4.8&filter="], :libram => ["4.7&filter="], :totem => ["4.9&filter="], :sigil => ["4.10&filter="], :wand => ["2.19&filter="],
   :dagger => ["2.15&filter="], :fist => ["2.13&filter="], :axe_one => ["2.0&filter="], :mace_one => ["2.4&filter="], :sword_one => ["2.7&filter="],
   :polearm => ["2.6&filter="], :staff => ["2.10&filter="], :axe_two => ["2.1&filter="], :mace_two => ["2.5&filter="], :sword_two => ["2.8&filter="],
-  :bow => ["2.2&filter="], :crossbow => ["2.18&filter="], :gun => ["2.3&filter="],
-  :thrown => ["2.16&filter="],
+  :bow => ["2.2&filter="], :crossbow => ["2.18&filter="], :gun => ["2.3&filter="], :thrown => ["2.16&filter="],
+  :relic => ["4.11&filter="], :wand => ["2.19&filter="],
 }
 types = [:cloth, :leather, :mail, :plate]
 types.each_index {|i| items[types[i]] = armor_slots.map {|s| "4.#{i+1}&filter=sl=#{s};"}}
@@ -32,16 +31,16 @@ class_item_types = {
   :death_knight => [:common, :cloth, :leather, :mail, :plate],
 }
 item_sets = {
-  :mage         => [:common,   :cloth,   :wand, :offhand, :shield, :dagger,        :staff,                                                     :sword_one],
-  :warlock      => [:common,   :cloth,   :wand, :offhand, :shield, :dagger,        :staff,                                                     :sword_one],
-  :priest       => [:common,   :cloth,   :wand, :offhand,          :dagger,        :staff,           :mace_one],
-  :druid        => [:common, :leather,   :idol, :offhand,          :dagger, :fist, :staff, :polearm, :mace_one, :mace_two],
-  :rogue        => [:common, :leather,          :offhand,          :dagger, :fist,                   :mace_one,                                :sword_one, :sword_two, :thrown, :gun, :bow, :crossbow],
-  :shaman       => [:common,    :mail,  :totem, :offhand, :shield, :dagger, :fist, :staff,           :mace_one, :mace_two, :axe_one, :axe_two],
-  :hunter       => [:common,    :mail,          :offhand,          :dagger, :fist, :staff, :polearm,                       :axe_one, :axe_two, :sword_one, :sword_two, :thrown, :gun, :bow, :crossbow],
-  :paladin      => [:common,   :plate, :libram, :offhand, :shield,                         :polearm, :mace_one, :mace_two, :axe_one, :axe_two, :sword_one, :sword_two],
-  :warrior      => [:common,   :plate,          :offhand, :shield, :dagger, :fist, :staff, :polearm, :mace_one, :mace_two, :axe_one, :axe_two, :sword_one, :sword_two, :thrown, :gun, :bow, :crossbow],
-  :death_knight => [:common,   :plate,  :sigil, :offhand,                                  :polearm, :mace_one, :mace_two, :axe_one, :axe_two, :sword_one, :sword_two],
+  :mage         => [:common,   :cloth,  :wand, :offhand, :shield, :dagger,        :staff,                                                     :sword_one],
+  :warlock      => [:common,   :cloth,  :wand, :offhand, :shield, :dagger,        :staff,                                                     :sword_one],
+  :priest       => [:common,   :cloth,  :wand, :offhand,          :dagger,        :staff,           :mace_one],
+  :druid        => [:common, :leather, :relic, :offhand,          :dagger, :fist, :staff, :polearm, :mace_one, :mace_two],
+  :rogue        => [:common, :leather,         :offhand,          :dagger, :fist,                   :mace_one,                                :sword_one, :sword_two, :thrown, :gun, :bow, :crossbow],
+  :shaman       => [:common,    :mail, :relic, :offhand, :shield, :dagger, :fist, :staff,           :mace_one, :mace_two, :axe_one, :axe_two],
+  :hunter       => [:common,    :mail,         :offhand,          :dagger, :fist, :staff, :polearm,                       :axe_one, :axe_two, :sword_one, :sword_two, :thrown, :gun, :bow, :crossbow],
+  :paladin      => [:common,   :plate, :relic, :offhand, :shield,                         :polearm, :mace_one, :mace_two, :axe_one, :axe_two, :sword_one, :sword_two],
+  :warrior      => [:common,   :plate,         :offhand, :shield, :dagger, :fist, :staff, :polearm, :mace_one, :mace_two, :axe_one, :axe_two, :sword_one, :sword_two, :thrown, :gun, :bow, :crossbow],
+  :death_knight => [:common,   :plate, :relic, :offhand,                                  :polearm, :mace_one, :mace_two, :axe_one, :axe_two, :sword_one, :sword_two],
 }
 
 wh.get_weight_filters.each_pair do |c,specs|
