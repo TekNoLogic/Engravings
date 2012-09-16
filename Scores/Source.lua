6fcba9e18e62663479549f179421d698b7ad9b4d
tekkub :bear:  <tekkub@gmail.com>
2012-09-16 17:54:01 -0600

    Try to suggest reforges

--- Scores/Source.lua
+++ Scores/Source.lua
@@ -2,6 +2,52 @@
 local myname, ns = ...
 
 
+local reforges = {}
+local sources = {
+	ITEM_MOD_CRIT_RATING_SHORT = true,
+	ITEM_MOD_DODGE_RATING_SHORT = true,
+	ITEM_MOD_EXPERTISE_RATING_SHORT = true,
+	ITEM_MOD_HASTE_RATING_SHORT = true,
+	ITEM_MOD_HIT_RATING_SHORT = true,
+	ITEM_MOD_MASTERY_RATING_SHORT = true,
+	ITEM_MOD_PARRY_RATING_SHORT = true,
+	ITEM_MOD_SPIRIT_SHORT = true,
+}
+local targets = {
+	ITEM_MOD_CRIT_RATING_SHORT = true,
+	ITEM_MOD_DODGE_RATING_SHORT = true,
+	ITEM_MOD_HASTE_RATING_SHORT = true,
+	ITEM_MOD_MASTERY_RATING_SHORT = true,
+	ITEM_MOD_PARRY_RATING_SHORT = true,
+	ITEM_MOD_SPIRIT_SHORT = true,
+}
+local function reforge(spec, id, stats, weights)
+	local target, source, lastdiff
+
+	for target_stat in pairs(targets) do
+		for source_stat in pairs(sources) do
+			if target_stat ~= source_stat and stats[source_stat]
+				and weights[target_stat] then
+
+				local usable = stats[source_stat] * 0.4
+				local source_weight = usable * (weights[source_stat] or 0)
+				local target_weight = usable * weights[target_stat]
+				local diff = target_weight - source_weight
+
+				if diff > 0 and (not lastdiff or diff > lastdiff) then
+					target, source, lastdiff = target_stat, source_stat, diff
+				end
+			end
+		end
+	end
+
+	if not target or not source then return end
+
+	reforges[spec] = reforges[spec] or {}
+	reforges[spec][id] = _G[source].. " -> ".. _G[target]
+end
+
+
 local stats, total_weights = {}, {}
 for spec,weights in pairs(ns.spec_weights) do
 	local total_weight = 0
@@ -17,6 +63,7 @@ for spec,weights in pairs(ns.spec_weights) do
 
 			wipe(stats)
 			stats = GetItemStats(link, stats)
+			reforge(spec, i, stats, weights)
 
 			local score = 0
 			for stat,val in pairs(stats) do
@@ -27,6 +74,12 @@ for spec,weights in pairs(ns.spec_weights) do
 
 			t[i] = score
 			return score
+		end
+	})
+
+	Engravings["Tek Reforge ("..spec.."):"] = setmetatable({}, {
+		__index = function(t,i)
+			return reforges[spec] and reforges[spec][i]
 		end
 	})
 
