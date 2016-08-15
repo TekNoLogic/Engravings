c08122def96ca4142567647069679fa96c12d74c
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-08-15 02:23:25 -0600

    Purge bad merge history from dataminer branch

--- Engravings.lua
+++ Engravings.lua
@@ -6,16 +6,10 @@ Engravings = {}
 local sources = Engravings
 local origs = {}
 local R, G, B = 1, 136/255, 0
-local db, dbpc, sortedtitles
-ns.fullLinkSources = {}
+local sortedtitles
+ns.dbname = "EngravingsDB2"
+ns.dbpcname = "EngravingsDBPC"
 
-
-local function initdb()
-	EngravingsDB2, EngravingsDBPC = EngravingsDB2 or {}, EngravingsDBPC or {}
-	for title in pairs(sources) do if title:match("^Wowhead score") and EngravingsDB2[title] then EngravingsDBPC[title], EngravingsDB2[title] = EngravingsDB2[title], nil end end
-	return EngravingsDB2, EngravingsDBPC
-end
-Engravings.initdb = initdb
 
 local function AddLines(frame, title, line, ...)
 	if not line then return end
@@ -24,7 +18,6 @@ local function AddLines(frame, title, line, ...)
 end
 
 local function OnTooltipSetItem(frame, ...)
-	if not db then db, dbpc = initdb() end
 	if not sortedtitles then
 		sortedtitles = {}
 		for title,data in pairs(sources) do table.insert(sortedtitles, title) end
@@ -33,12 +26,11 @@ local function OnTooltipSetItem(frame, ...)
 
 	local name, link = frame:GetItem()
 	if link then
-		local id = tonumber(link:match("item:(%d+):"))
+		local id = ns.ids[link]
 		for i,title in pairs(sortedtitles) do
 			local data = sources[title]
 			local key = id
-			if ns.fullLinkSources[title] then key = link end
-			if not (db[title] or dbpc[title]) and data[key] then AddLines(frame, title, string.split("`", data[key])) end
+			if not (ns.db[title] or ns.dbpc[title]) and data[key] then AddLines(frame, title, string.split("`", data[key])) end
 		end
 	end
 	if origs[frame] then return origs[frame](frame, ...) end
@@ -48,21 +40,4 @@ end
 for _,frame in pairs{GameTooltip, ItemRefTooltip, ShoppingTooltip1, ShoppingTooltip2} do
 	origs[frame] = frame:GetScript("OnTooltipSetItem")
 	frame:SetScript("OnTooltipSetItem", OnTooltipSetItem)
-end
-
-
----------------------------------
---      Wowhead generator      --
----------------------------------
-
-function EngravingsGenerateWowheadSet(spec, data)
-	local values = setmetatable({}, {
-		__index = function(t,i)
-			local v = data:match("\n"..i.." ([^\n]+)\n")
-			if v then t[i] = v; return v
-			else t[i] = 0; return 0 end
-		end
-	})
-
-	ns.GenerateScoreSet("Wowhead score ("..spec.."):", values)
 end
