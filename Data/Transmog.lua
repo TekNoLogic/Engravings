b6ca3cb300960537d16cd22fe436fa100a995bcf
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-09-06 22:04:56 -0600

    Refresh appearances when we learn a new one

--- Data/Transmog.lua
+++ Data/Transmog.lua
@@ -4,7 +4,13 @@ local myname, ns = ...
 
 function ns.OnLoads.Transmog()
 	ns.db.appearances = ns.db.appearances or {}
+	ns.TRANSMOG_COLLECTION_UPDATED()
+end
 
+
+local data = {}
+function ns.TRANSMOG_COLLECTION_UPDATED()
+	wipe(data)
 	C_TransmogCollection.ClearSearch()
 	for id=1,28 do
 		appearances = C_TransmogCollection.GetCategoryAppearances(id)
@@ -15,15 +21,17 @@ function ns.OnLoads.Transmog()
 		end
 	end
 end
+ns.RegisterEvent("TRANSMOG_COLLECTION_UPDATED")
 
 
 local COLLECTED = GREEN_FONT_COLOR_CODE.."Collected"
 local NOT_COLLECTED = "|cff87aaffNot collected"
-Engravings["Transmog:"] = setmetatable({}, {
+Engravings["Transmog:"] = setmetatable(data, {
 	__index = function(t,i)
 		local visualid = Engravings["VisualID:"][i]
 		if not visualid then return end
 
+		if ns.db.appearances[visualid] then
 			t[i] = COLLECTED
 			return COLLECTED
 		else
