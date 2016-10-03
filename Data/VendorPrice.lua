7482ff1331d640a58feed6eb09d177901bc8c18e
tekkub ʕ ´ᴥ` ʔ <git@tekkub.net>
2016-10-03 04:02:03 -0600

    give a more descriptive error

--- Data/VendorPrice.lua
+++ Data/VendorPrice.lua
@@ -4,7 +4,9 @@ local myname, ns = ...
 
 Engravings["Vendor:"] = setmetatable({}, {
 	__index = function(t,i)
-		local _, _, _, _, _, _, _, stack, _, _, price = GetItemInfo(i)
+		local status, _, _, _, _, _, _, _, stack, _, _, price = pcall(GetItemInfo, i)
+		assert(status, ("GetItemInfo failed for %q"):format(i or "nil"))
+
 		if price and ((stack or 0) > 1) then
 			price = ns.GSC(price)
 			t[i] = price
